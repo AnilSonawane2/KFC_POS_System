adb1e1720d8445261385d63c4853fdc798b04f1a
Anil Sonawane <141408939+AnilSonawane2@users.noreply.github.com>
2024-07-20 11:13:39 +0530

    Add files via upload

---
+++ src/simple_pos/Simple_POS.java
@@ -0,0 +1,10 @@
+package simple_pos;
+
+public class Simple_POS {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }  
+}
